f61c335df5a4b30105e2647bcba0104c85275b01
David Coomer <dmcoomer@hotmail.com>
2022-04-02 21:43:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+for (let i = 0; i < record.length; i++) {
+  if (record[i].result === 'W') {
+    const firstWin = record.find( ({result}) => result === 'W');
+    return firstWin.year
+  }
+}
+}
+
+
